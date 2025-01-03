dbfdfe93a08efaab4acd78f4dbcd283c7c46c8c4
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-01-03 14:04:21 +0100

    added job_handler models to django admin panel

--- orchestrator/control_server/job_handler/admin.py
+++ orchestrator/control_server/job_handler/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Job, EnviromentVariable, Agent, Container, AgentCheck
 
 # Register your models here.
+admin.site.register(Job)
+admin.site.register(EnviromentVariable)
+admin.site.register(Agent)
+admin.site.register(Container)
+admin.site.register(AgentCheck)

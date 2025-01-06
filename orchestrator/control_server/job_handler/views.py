5d4d3bb5574360e598b6fd22d0c5008e84f99cec
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-01-06 18:10:02 +0100

    partially implemented create job function

--- orchestrator/control_server/job_handler/views.py
+++ orchestrator/control_server/job_handler/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Job
+from .forms import JobForm, EnviromentVariableForm, SampleForm, BaseEnviromentVariableFormset
+from django.forms import modelformset_factory
 
-# Create your views here.
 
+# TODO: use this in the index page later:
+# <li><a href="{% url 'detail' question.id %}">{{ question.question_text }}</a></li>
+# path("<int:question_id>/", views.detail, name="detail"),
+# See https://docs.djangoproject.com/en/5.1/intro/tutorial03/
 # Lists all the available jobs and is also the root page
 def index(request):
+    """The index page lists all the available jobs and their current state. It is also the root page of the application."""
     jobs = Job.objects.all()
 
     context = {
@@ -14,27 +20,51 @@ def index(request):
     }
     return render(request, 'job_handler/index.html', context)
 
-# TODO: use this in the index page later:
-# <li><a href="{% url 'detail' question.id %}">{{ question.question_text }}</a></li>
-# path("<int:question_id>/", views.detail, name="detail"),
-# See https://docs.djangoproject.com/en/5.1/intro/tutorial03/
 
-
-# Creates a new job and redirects to the index page (includes also exception handling)
 def create_job(request):
-    try:
-        job = Job(name=request.POST['name'], latest_operation=request.POST['latest_operation'], container_image_name=request.POST['container_image_name'], container_number=request.POST['container_number'], container_cpu_limit=request.POST['container_cpu_limit'], container_memory_limit=request.POST['container_memory_limit'], computation_duration_in_seconds=request.POST['computation_duration_in_seconds'], kafka_timestamp=request.POST['kafka_timestamp'])
-        job.save()
-    except (KeyError, Job.DoesNotExist):
-        return render(request, 'job_handler/index.html', {
-            'error_message': "You didn't provide all the necessary fields.",
-        })
-    else:
-        return HttpResponseRedirect(reverse('index'))
+    """This view is responsible for creating a new job and the corresponding enviroment variables. It handles both GET and POST requests."""
+
+    current_extra_forms = 1
+    extra_form_number = 1
+    EnviromentVariableFormSet = modelformset_factory(EnviromentVariableForm, formset=BaseEnviromentVariableFormset, extra=extra_form_number)
+
+    # Everithing else than a POST request
+    if request.method != 'POST':
+        # GET request
+        enviroment_variable_formset = EnviromentVariableFormSet(prefix='env')
+        job_form = JobForm()
+        context = {
+            'job_form': job_form,
+            'env_formset': enviroment_variable_formset,
+            'extra_forms': current_extra_forms,
+        }
+        return render(request, 'job_handler/new_job.html', context=context)
     
-# filepath: orchestrator/control_server/job_handler/views.py
-from django.shortcuts import render
-from .forms import SampleForm
+    # Only POST requests
+    job_form = JobForm(request.POST)
+    enviroment_variable_formset = EnviromentVariableFormSet(request.POST, prefix='env')
+
+    # If the data in the existing form set is valid, the existing data stays in the formset and a new form is added
+    if "add_env" in request.POST:
+        if enviroment_variable_formset.is_valid():
+            EnviromentVariableFormSet = modelformset_factory(EnviromentVariableForm, formset=BaseEnviromentVariableFormset, extra=extra_form_number)
+            updated_enviroment_variable_formset = EnviromentVariableFormSet(initial=[form.cleaned_data for form in enviroment_variable_formset], prefix='env')
+            current_extra_forms += 1
+        return render(request, 'job_handler/new_job.html', {
+            'job_form': job_form,
+            'env_formset': updated_enviroment_variable_formset,
+            'extra_forms': current_extra_forms,
+        })
+
+    # If the form data is valid, it is saved in the database
+    if job_form.is_valid() and enviroment_variable_formset.is_valid():
+        # The job form data is only saved as a job instance, not in the database
+        job = job_form.save(commit=False)
+
+
+        # Save enviroment variables in database
+
+        
 
 def show_bootstrap(request):
     form = SampleForm()

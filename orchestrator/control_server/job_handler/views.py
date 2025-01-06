4403b4ab2d248f3ba5327acbf2f1917a21a96e7c
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-01-07 00:01:11 +0100

    fixed bug: new job env_formset does not load existing objects from the db

--- orchestrator/control_server/job_handler/views.py
+++ orchestrator/control_server/job_handler/views.py
@@ -30,7 +30,7 @@ def create_job(request):
     if request.method != 'POST':
         # GET request
         EnviromentVariableFormSet = modelformset_factory(model=EnviromentVariable, form=EnviromentVariableForm, formset=BaseEnviromentVariableFormset, extra=1)
-        enviroment_variable_formset = EnviromentVariableFormSet(prefix='env')
+        enviroment_variable_formset = EnviromentVariableFormSet(prefix='env', queryset=EnviromentVariable.objects.none())
         job_form = JobForm()
         context = {
             'job_form': job_form,
@@ -43,21 +43,21 @@ def create_job(request):
     current_extra_forms = int(request.POST.get('extra_forms'))
     job_form = JobForm(request.POST)
     EnviromentVariableFormSet = modelformset_factory(model=EnviromentVariable, form=EnviromentVariableForm, formset=BaseEnviromentVariableFormset, extra=current_extra_forms)
-    enviroment_variable_formset = EnviromentVariableFormSet(request.POST, prefix='env')
+    enviroment_variable_formset = EnviromentVariableFormSet(request.POST, prefix='env', queryset=EnviromentVariable.objects.none())
 
     # If the data in the existing form set is valid, the existing data stays in the formset and a new form is added
     if "add_env" in request.POST:
         if enviroment_variable_formset.is_valid():
-            current_extra_forms = int(request.POST.get('extra_forms'))
-            print("Adding a new form")
             current_extra_forms += 1
             EnviromentVariableFormSet = modelformset_factory(model=EnviromentVariable, form=EnviromentVariableForm, formset=BaseEnviromentVariableFormset, extra=current_extra_forms)
-            enviroment_variable_formset = EnviromentVariableFormSet(initial=[form.cleaned_data for form in enviroment_variable_formset], prefix='env')
-        return render(request, 'job_handler/new_job.html', {
+            enviroment_variable_formset = EnviromentVariableFormSet(initial=[form.cleaned_data for form in enviroment_variable_formset], prefix='env', queryset=EnviromentVariable.objects.none())
+        
+        context = {
             'job_form': job_form,
             'env_formset': enviroment_variable_formset,
             'extra_forms': current_extra_forms,
-        })
+        }
+        return render(request, 'job_handler/new_job.html', context=context)
 
 
     # Save form data in the database

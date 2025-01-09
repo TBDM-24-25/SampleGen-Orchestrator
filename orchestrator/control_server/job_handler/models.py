ba8375cede4fd547477c8516bd309a0edf1a71c1
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-01-09 19:50:27 +0100

    added agent foreign key to job model

--- orchestrator/control_server/job_handler/models.py
+++ orchestrator/control_server/job_handler/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+class Agent(models.Model):
+    status = models.CharField(max_length=200)
+    kafka_timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Job(models.Model):
     name = models.CharField(max_length=200)
     latest_operation = models.CharField(max_length=200)
@@ -11,10 +18,7 @@ class Job(models.Model):
     container_memory_limit = models.CharField(max_length=200)
     computation_duration_in_seconds = models.IntegerField()
     kafka_timestamp = models.DateTimeField(null=True)
-    # user ID foreign key, will be added later when authentication is implemented
-    # TODO: implement authentication
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # TODO: enviroment variables must be mandatory when creating a job
+    agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
  
@@ -23,13 +27,6 @@ class EnviromentVariable(models.Model):
     variable_name = models.CharField(max_length=200, null=False)
     variable_value = models.CharField(max_length=200, null=False)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Agent(models.Model):
-    status = models.CharField(max_length=200)
-    kafka_timestamp = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

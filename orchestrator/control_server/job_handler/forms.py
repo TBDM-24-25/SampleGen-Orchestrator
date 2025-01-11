230c8beaaa8b5eb3f8619569e3ef01e57aba13fa
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-01-11 01:50:52 +0100

    added: container_memory_limit in ui, container_compute default value=300, iot_data_kafka_topic in model and frontend, value range validation for container_resources

--- orchestrator/control_server/job_handler/forms.py
+++ orchestrator/control_server/job_handler/forms.py
@@ -8,21 +8,26 @@ from django.utils.translation import gettext_lazy as _
 
 class JobForm(ModelForm):
     class Meta:
+        # TODO: Add memory limit field
         model = Job
-        fields = ["name", "container_image_name", "container_number", "container_cpu_limit", "computation_duration_in_seconds"]
+        fields = ["name", "container_image_name", "container_number", "container_cpu_limit", "container_memory_limit_in_mb", "computation_duration_in_seconds", "iot_data_kafka_topic"]
         labels = {
             "name": _("Name"),
             "container_image_name": _("Container image name"),
             "container_number": _("Container number"),
             "container_cpu_limit": _("Container CPU limit"),
+            "container_memory_limit_in_mb": _("Container memory limit in Megabytes"),
             "computation_duration_in_seconds": _("Computation duration"),
+            "iot_data_kafka_topic": _("Kafka topic"),
         }
         help_texts = {
             "name": _("How you would like to name your Job"),
             "container_image_name": _("Provide the container image name use in your container registry."),
             "container_number": _("the number of containers to deploy"),
             "container_cpu_limit": _("The amount of CPU the container can use. Please provide a floating point number."),
+            "container_memory_limit_in_mb": _("The amount of memory the container can use in megabytes. The value must be between 512 and 2048 Megabytes."),
             "computation_duration_in_seconds": _("The duration of computation measured in seconds"),
+            "iot_data_kafka_topic": _("The Kafka topic where the IoT data is published."),
         }
 
 class EnviromentVariableForm(ModelForm):
--- orchestrator/control_server/job_handler/models.py
+++ orchestrator/control_server/job_handler/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -14,14 +15,43 @@ class Job(models.Model):
     latest_operation = models.CharField(max_length=200)
     container_image_name = models.CharField(max_length=200)
     container_number = models.IntegerField(default=1)
-    container_cpu_limit = models.FloatField(null=False)
-    container_memory_limit = models.CharField(max_length=200)
-    computation_duration_in_seconds = models.IntegerField()
+    container_cpu_limit = models.FloatField(null=False, default=0.3)
+    container_memory_limit_in_mb = models.IntegerField(default=512)
+    computation_duration_in_seconds = models.IntegerField(default=300)
+    iot_data_kafka_topic = models.CharField(max_length=200)
     kafka_timestamp = models.DateTimeField(null=True)
     agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
- 
+    updated_at = models.DateTimeField(auto_now=True)   
+
+    def clean(self):
+        """Validate the model fields."""
+        super().clean()
+        validation_errors = {}
+
+        if self.container_cpu_limit < 0.1:
+            validation_errors['container_cpu_limit'] = "Container CPU limit must be greater than 0.1"
+        if self.container_cpu_limit > 2.0:
+            validation_errors['container_cpu_limit'] = "Container CPU limit must be less than 2.0"
+
+        if self.container_number < 1:
+            validation_errors['container_number'] = "Container number must be at least 1"
+        if self.container_number > 10:
+            validation_errors['container_number'] = "Container number must be at most 10"
+
+        if self.container_memory_limit_in_mb < 512:
+            validation_errors['container_memory_limit_in_mb'] = "Container memory limit must be at least 512 MB"
+        if self.container_memory_limit_in_mb > 2048:
+            validation_errors['container_memory_limit_in_mb'] = "Container memory limit must be at most 2048 MB"
+
+        if self.computation_duration_in_seconds < 60:
+            validation_errors['computation_duration_in_seconds'] = "Computation duration must be at least 60 seconds"
+        if self.computation_duration_in_seconds > 600:
+            validation_errors['computation_duration_in_seconds'] = "Computation duration must be at most 600 seconds"
+
+        if validation_errors:
+            raise ValidationError(validation_errors)
+
 
 class EnviromentVariable(models.Model):
     variable_name = models.CharField(max_length=200, null=False)
